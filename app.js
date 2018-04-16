1c5edf945f4aac0a5de0032ca56f1f467839a5df
galagarza89 <davegala89@gmail.com>
2018-04-16 10:36:03 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public/'));
 
 app.get('/', (req, res) => {
-	res.render('./public/index.html');
+	res.render('index.html');
 });
 
 
